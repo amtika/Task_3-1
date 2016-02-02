2959062a739143c180ff6c63ad0892edccd3ce40
amtika <amtika27@gmail.com>
2016-02-02 23:52:55 +0700

    Update Member.java

--- Exercise_2/Member.java
+++ Exercise_2/Member.java
@@ -1,1 +1,44 @@
+public class Member {
+    private String name;
+    private String specialization;
+    private int projectWorked=0;
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public Member(String name, String specialization) {
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public int getProjectWorked() {
+        projectWorked++;
+        return projectWorked;
+    }
+
+    public void setProjectWorked(int projectWorked) {
+        this.projectWorked = projectWorked;
+    }
+    @Override
+    public String toString() {
+        return "Member " + name + " specialized in " + specialization + " and already worked on " + projectWorked + " projec(s)";
+    }
+}
 
