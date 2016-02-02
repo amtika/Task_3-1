b4cf90814158dfd3203a45c8043b0a6571eebc15
amtika <amtika27@gmail.com>
2016-02-02 23:11:29 +0700

    Update Project.java

--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,35 @@
+public class Project {
 
+    private String projectName;
+    private Member[] teamMember = new Member[5];
+    private int nTeam = 0;
+    private boolean releaseStatus = false;
+
+    public Project(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public void addMember(Member m) {
+        teamMember[nTeam] = m;
+        nTeam++;
+    }
+
+    public boolean isReleased() {
+        return releaseStatus ;
+    }
+
+    public void releaseApp() {
+        releaseStatus = true;
+    }
+
+    public String toString() {
+        String str;
+        if (releaseStatus = true) {
+            str = "released";
+        } else {
+            str = "progress";
+        }
+        return "Project " + projectName + " status is in " + str + " with team member of " + nTeam;
+    }
+}
+        
