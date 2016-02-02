29a33464f1eff8d56a8eaf5bcc9bf3c24a02b691
amtika <amtika27@gmail.com>
2016-02-02 23:11:58 +0700

    Update StartUp.java

--- Exercise_2/StartUp.java
+++ Exercise_2/StartUp.java
@@ -1,1 +1,40 @@
+public class StartUp {
+	private Member[] member = new Member[50];
+	private Project[] projectList = new Project[50];
+	private int nTeam=0;
+	private int nProject =0;
+	   
+    public void addMember(Member m) {
+        member[nTeam] = m;
+        nTeam++;
+    }    
+    
+    public Member getMember(int id) {
+        return member[id];
+    }
+    
+    public void createNewProject(String projectName) {
+        Project p = new Project(projectName);
+        projectList[nProject] = p;
+        nProject++;
+    }
+    
+    public Project getProject(int id) {
+        return projectList[id];
+    }
+    
+    public void setProjectMember(Project p, Member m) {
+        int x = m.getProjectWorked();      
+        p.addMember(m);        
+        m.setProjectWorked(x);
+    }
+    
+    public void releaseProject(Project p) {
+        p.releaseApp();
+    }
+    
+    public int getNumReleasedProject() {
+        return nProject;
+    }
+}
 
