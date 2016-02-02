c295bae6d784e0447911db5835116bc274985e83
amtika <amtika27@gmail.com>
2016-02-02 20:08:00 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,32 @@
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application (String appName, int appSize) {
+		this.appName = appName;
+		this.appSize = appSize;
+		
+	}
 
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+
+	public String getAppName() {
+		return appName;
+	}
+	
+	public void setAppSize() {
+		this.appSize = appSize;
+	}
+	
+	public int getAppSize() {
+		return appSize;
+	}
+	
+	public String toString() {
+		return "Application Nama : "+appName+", with size : "+" Mb";
+	}
+	
+	
+}
